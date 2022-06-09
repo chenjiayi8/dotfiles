fbf3c39d0f84e1b4ba7f719941efce58c76cd10c
Jiayi Chen <chenjiayi_344@hotmail.com>
2022-06-09 16:35:48 +0200

    Update config_nvim.sh

--- neovim/config_nvim.sh
+++ neovim/config_nvim.sh
@@ -3,14 +3,13 @@
 ## add repository
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt install neovim
+sudo apt install neovim -y
 mkdir -p ~/.config/nvim/
 mkdir ~/.vim/autoload/
 cp init.vim ~/.config/nvim/init.vim
 cp plug.vim ~/.vim/autoload/plug.vim
 cp .vimrc ~/.vimrc
 
-
-
-
+## PlugInstall 
+nvim +PlugInstall +qall
 
